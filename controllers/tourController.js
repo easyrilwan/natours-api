4acc94d93392b98f78fa19b83457c8db840d2af2
Rilwan Idowu Suleiman <easyrilwan@gmail.com>
2025-06-30 01:23:08 +0100

    setting up eslint and prettier

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
 );
 
 exports.checkID = (req, res, next, val) => {
@@ -70,7 +70,7 @@ exports.createTour = (req, res) => {
           tour: newTour,
         },
       });
-    }
+    },
   );
 };
 
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
-
-const app = require('./app');
 
 // console.log(process.env);
 
